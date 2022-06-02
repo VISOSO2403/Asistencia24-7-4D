7657f3c7cfcc6ac16837817740d20b77c75aefd3
Oliver Visoso <ovisoso@ucol.mx>
2022-06-01 22:21:47 -0500

    modificacion a contacto (aun no terminado)

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import Footer from '../components/Footer'
 
 const Contacto = () => {
   return (
-    <div>Contacto</div>
+    <div>
+      <section className='pt-20 pb-48'>
+        <div className='container mx-auto px-4'>
+          <div className='flex flex-wrap justify-center text-center mb-24'>
+            <div className='w-full lg:w-6/12 px-4'>
+              <h2 className='text-4x1 font-semibold'>los coño de su madre</h2>
+              <p className='text-lg leading-relaxed m-4 text-blueGray-500'>que mas quieres de mi asjajsajs sexoo
+              </p>
+            </div>
+
+          </div>
+
+        </div>
+      </section>
+      < Footer />
+    </div>
   )
 }
 
