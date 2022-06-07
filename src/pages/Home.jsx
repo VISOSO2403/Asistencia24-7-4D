90d6a624414a0da8c266660a0c4a31a9018db9e8
Oliver Visoso <ovisoso@ucol.mx>
2022-06-06 21:45:21 -0500

    Links modificados en Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Footer from "../components/Footer";
 const Home = () => {
 
   return (
-    <div>
+    <div className="text-white">
       {/* Inyeccion del NavBar.jsx */}
       <NavBar />
       <div className="font-bold 2xl:text-3xl sm:text-2xl items-center text-center w-full mx-auto overflow-hidden pt-36  pb-10 ">
@@ -13,14 +13,14 @@ const Home = () => {
           <hr />
           <br />
           <section className=" flex flex-col items-center pb-5">
-          <iframe width="50%" src="https://stem.ubidots.com/app/dashboards/public/widget/YXe1XkCbfzFBlJcUDjqFNz_iWoYr_5FPyfUakxHoOHE?embed=true"></iframe>
+          <iframe width="50%" height="450"  src="https://industrial.ubidots.com/app/dashboards/public/widget/jcwwM437Wm6aUA5IBDQa7IcWjHDfzwAsyglXx3ZE6vA?embed=true"></iframe>
           </section>
           <section className="flex flex-col items-center">
             {/* BMP */}
             <iframe 
               width="90%" 
               height="500" 
-              src="https://stem.ubidots.com/app/dashboards/public/widget/hXTEgLzRARSel0n1r3gVAx-gFqXbJQ-xMVI-JHBhE28?embed=true"></iframe>
+              src="https://industrial.ubidots.com/app/dashboards/public/widget/Fo-FRKbItBlZrhjeb3TGFeL7H-ySkZh5EG7K8IQtq7M?embed=true"></iframe>
           </section>
 
           <section className="p-20 grid grid-cols-2 gap-10 justify-center">
@@ -31,7 +31,7 @@ const Home = () => {
                 className=""
                 width="100%" 
                 height="300"
-                src="https://stem.ubidots.com/app/dashboards/public/widget/3e7HA4vuQBMRX4fxj23AvKNfxHmGhWNAOEDPG4e_ZaI?embed=true" 
+                src="https://industrial.ubidots.com/app/dashboards/public/widget/70Lp0g0z8zGTM3K8Kywg4xRq5Wwcpw4CfybMn-AtTvM?embed=true" 
               />
               </h2>
 
@@ -40,7 +40,7 @@ const Home = () => {
               <iframe 
                 width="100%" 
                 height="300" 
-                src="https://stem.ubidots.com/app/dashboards/public/widget/9vdfFDqpc5-Csa9U8JDkn-VGQ-6Mqd5p1OdEENyOzHQ?embed=true" 
+                src="https://industrial.ubidots.com/app/dashboards/public/widget/RJI5AWezSbIbOo__jqSUBRMTGCAB2NYnTgGXpqnGKow?embed=true" 
               />
               </h2>
           </section>
